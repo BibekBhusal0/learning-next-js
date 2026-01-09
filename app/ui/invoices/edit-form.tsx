756f2a776644b7ac71dd0ab3ec45453b020e4a30
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-09 21:36:05 +0545

    compleate chapter 15 🎉

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -19,7 +19,7 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const update = (formData: FormData) => updateInvoice(invoice.id, formData);
+  const update = (formData: FormData) => { updateInvoice(invoice.id, formData) };
   const initialState: State = {message: null , errors: {}}
   const [_, formAction] = useActionState(update, initialState)
 
